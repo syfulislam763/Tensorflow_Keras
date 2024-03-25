d3b745110fa1964527199d1d27dc0e51b0f50812
syfulislam763 <mahimkhan763@gmail.com>
2024-03-26 02:55:07 +0600

    matrix product

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow as tf
 import math
+import numpy as np
 
 
-print(tf.__version__)
+a = np.array([
+        [3, 4, 5],
+        [1, 2, 3],
+        [6, 7, 8]
+    ])
 
-print(math.exp(1))
-
+b = a*5
+print(b)
